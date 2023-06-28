5508e594054dbc9ccac5af2dc0caae94ff926a5d
danilkk <ezzfvkoko@gmail.com>
2023-06-28 15:12:16 +0300

    init controller and service + add endpoint POST gauss method

--- src/main/java/com/example/matrixapi/controllers/MatrixController.java
+++ src/main/java/com/example/matrixapi/controllers/MatrixController.java
@@ -1,9 +1,13 @@
 package com.example.matrixapi.controllers;
 
+import com.example.matrixapi.dto.GaussMethodRequestDTO;
+import com.example.matrixapi.dto.GaussMethodResponseDTO;
 import com.example.matrixapi.services.MatrixService;
+import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -11,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MatrixController {
 
     private final MatrixService matrixService;
+    private final Gson gson;
 
-
+    @PostMapping("/gauss")
+    public ResponseEntity gauss(@RequestBody GaussMethodRequestDTO gaussMethodDTO){
+        return ResponseEntity.ok(matrixService.gauss(gaussMethodDTO));
+    }
 }
--- src/main/java/com/example/matrixapi/services/MatrixService.java
+++ src/main/java/com/example/matrixapi/services/MatrixService.java
@@ -1,7 +1,21 @@
 package com.example.matrixapi.services;
 
+import com.example.matrixapi.dto.GaussMethodRequestDTO;
+import com.example.matrixapi.dto.GaussMethodResponseDTO;
+import com.example.matrixapi.objects.matrix.Matrix;
+import lombok.Data;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@Data
 public class MatrixService {
+    private final ModelMapper modelMapper;
+
+    public GaussMethodResponseDTO gauss(GaussMethodRequestDTO gaussMethodDTO) {
+        Matrix matrix = new Matrix(gaussMethodDTO.getMatrix());
+        matrix.gaussianElimination();
+
+        return modelMapper.map(matrix, GaussMethodResponseDTO.class);
+    }
 }
