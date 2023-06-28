2bf51a760578e71817ac86bd9944d979d8c3ce18
danilkk <ezzfvkoko@gmail.com>
2023-06-28 15:12:36 +0300

    add gauss method DTO

---
+++ src/main/java/com/example/matrixapi/dto/GaussMethodRequestDTO.java
@@ -0,0 +1,8 @@
+package com.example.matrixapi.dto;
+
+import lombok.Data;
+
+@Data
+public class GaussMethodRequestDTO {
+    private double[][] matrix;
+}
---
+++ src/main/java/com/example/matrixapi/dto/GaussMethodResponseDTO.java
@@ -0,0 +1,8 @@
+package com.example.matrixapi.dto;
+
+import lombok.Data;
+
+@Data
+public class GaussMethodResponseDTO {
+    private double[] variables;
+}
