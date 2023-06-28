8c9cde406307450a7f0094489e7631cb7d793467
danilkk <ezzfvkoko@gmail.com>
2023-06-28 15:12:24 +0300

    add gson and modelmapper

--- src/main/java/com/example/matrixapi/MatrixApiApplication.java
+++ src/main/java/com/example/matrixapi/MatrixApiApplication.java
@@ -1,7 +1,10 @@
 package com.example.matrixapi;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MatrixApiApplication {
@@ -9,5 +12,15 @@ public class MatrixApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MatrixApiApplication.class, args);
 	}
+
+	@Bean
+	public Gson gson() {
+		return new Gson();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	};
 
 }
